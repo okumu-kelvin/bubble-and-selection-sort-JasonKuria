2b931a5a8e3bb946c6bd8a48893a939c15d5ac5b
kelvin okumu <105194367+kelvinokumu@users.noreply.github.com>
2025-06-26 10:37:55 +0300

    Update bubble_sort.py

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -5,7 +5,8 @@ def bubble_sort(unsorted_list):
         for j in range(0, n - i - 1):
             if unsorted_list[j] > unsorted_list[j + 1]:
                 unsorted_list[j], unsorted_list[j + 1] = unsorted_list[j + 1], unsorted_list[j]
-    pass
+    
+    return unsorted_list
 
 my_list = [6,2,44,5,59,3,1,3]
 bubble_sort(my_list)
